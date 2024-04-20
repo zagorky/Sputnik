22cb4b8dc6337dfe21e665e8244846698e067a33
Daria <zagorkiynahyi@gmail.com>
2024-04-20 13:56:05 +0300

    мелкие правки по внешнему виду

--- js/accordion.js
+++ js/accordion.js
@@ -1,14 +1,11 @@
 let acc = document.getElementsByClassName("accordion");
 let i;
 
-
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        
-        this.classList.toggle("active");
-
-       
+        this.classList.toggle("active");   
         let panel = this.nextElementSibling;
+
         if (panel.style.display === "block") {
             panel.style.display = "none";
         } else {
